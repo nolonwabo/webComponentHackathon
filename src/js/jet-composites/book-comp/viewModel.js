6e29f34bd14714eb3c77646931e6f531b07a832d
sivuyisiwe <sivuyisiwequse0@gmail.com>
2017-11-22 08:57:08 +0200

    commented out

--- src/js/jet-composites/book-comp/viewModel.js
+++ src/js/jet-composites/book-comp/viewModel.js
@@ -10,10 +10,7 @@ define(
         var self = this;
         self.composite = context.element;
         //Example observable
-        // self.messageText = ko.observable('Hello from Example Component');
-        // self.title = ko.observable("Beauty and the Beast");
-        // self.description = ko.observable("Belle, a bright, beautiful and independent young woman, is taken prisoner by a beast in its castle. Despite her fears, she befriends the castle's enchanted staff and learns to look beyond the beast's hideous exterior, allowing her to recognize the kind heart and soul of the true prince that hides on the inside.")
-
+        
         context.props.then(function (propertyMap) {
             //Store a reference to the properties for any later use
             self.properties = propertyMap;
