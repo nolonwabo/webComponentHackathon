72ee248cdddca0ef7839b9be93e41bad0b8248b6
Amandankosiyane <amandankosiyane.etafeni@gmail.com>
2017-11-22 09:19:48 +0200

    first commit

--- src/js/viewModels/dashboard.js
+++ src/js/viewModels/dashboard.js
@@ -1,95 +1,47 @@
 define(['ojs/ojcore', 'knockout', 'jquery', 'ais', 'ds', 'jet-composites/filter-table/loader', 'jet-composites/overview-card/loader'],
   function (oj, ko, $, ais, ds) {
 
-    function DashboardViewModel() {
-      var self = this;
-      // check ds is initialized
-      self.itemsArray = ko.observable([]);
-      // for web component pattern
-      // data service delivers for filter-table component --- data prop requirements
 
 
-      self.itemData = ds.getItemsStatic();
-      ds.init().then(function (data) {
-        console.log(data)
-        // for JQUERY injection pattern - below in hamdleAttached method
+  // return showAllBooksViewModel();
 
 
-        // for KO Pattern - fetchdata into observable
+// var ViewModel = function(){
 
 
-        ds.getItemsAPI().then(function (data2) {
-          console.log(JSON.stringify(data2.rows))
-          self.itemsArray(data2.rows);
+// Model////
+function userModel(result) {
+  var self = this;
+  this.name = ko.observable(result[0].name); /// give me error undefined before  the ajax call and after ajax call i get the value in result
+  this.userName = ko.observable();
 
-        });
+}
 
+/////View Model////
+var result
+var userDetailsViewModel = function(result) {
+  self = this;
+  self.user = ko.observable(new userModel(result));
+};
+var mainUserDetailsViewModel = new userDetailsViewModel(result);
+ko.applyBindings(mainUserDetailsViewModel);
 
+////ajax called on the page load ////
+$.ajax({
+  type: "GET",
+  dataType: "json",
+  url: baseUrl + 'https://www.googleapis.com/books/v1/volumes/zyTCAlFPjgYC',
+  data: jason.strigfy(),
+  success: function(data) {
+    result = data;
+    ////I am getting in result json data object 0=["name":"nnnn","Username":"mmmmmm"],
+    ////  i am passing this result to ViewModel and to Usermodel Constructor//
+    mainUserDetailsViewModel.user(new userModel(result));
 
-      })
-
-
-      // event handler - provides jquery and jquery ui event objects
-      self.handleRowClick = function (evt, ui) {
-        console.log($(this));
-        //console.log("row clicked == "+evt.target.id); // this is table ID
-        console.log("row clicked == " + JSON.stringify(ui)); // startIndex holds row
-
-      };
-
-      // setup obeservables for overview-card
-      self.cachedNumber = ko.observable({
-        count: 5
-      });
-
-      // component life cycle methods
-      self.handleActivated = function (info) {
-        // Implement if needed
-        console.log("dashboard viewModel activated : " + info); // this is the entire view html object at this state in the lifecycle
-      };
-
-      self.handleAttached = function (info) {
-
-
-        // DOM is ready
-        // can use jQuery to inject our table html
-        // const tableEl = $("#myTarget");
-        // $.each(self.itemsArray(), function (i,o){
-        //   //console.log(JSON.stringify(o))
-        //   const kys = Object.keys(o);
-        //   $.each(kys, function(idx,obj) {
-        //     tableEl.append('<tr><td>'+obj+'</td><td>'+o[obj]+'</td></tr>')
-        //   });
-
-        // })
-
-
-        // inject custom components
-        // for(var x=0;x<15;x++){
-        //   $('#appHolder').append('<overview-card name="name" maincolor="#c1c1c1" highlight="#B3CD62" observables="{{cachedNumber}}" clicked="{{handleRowClick}}" icon="home-icon-24"></overview-card>')
-        // }
-
-
-      };
-
-
-      self.handleBindingsApplied = function (info) {
-        // Implement if needed
-
-      };
-
-
-      self.handleDetached = function (info) {
-        // Implement if needed
-
-      };
-    }
-
-    /*
-     * Returns a constructor for the ViewModel so that the ViewModel is constructed
-     * each time the view is displayed.  Return an instance of the ViewModel if
-     * only one instance of the ViewModel is needed.
-     */
-    return new DashboardViewModel();
+  },
+  error: function(error) {
+    jsonValue = jQuery.parseJSON(error.responseText);
+    //jError('An error has occurred while saving the new part    source: ' + jsonValue, { TimeShown: 3000 });
   }
-);
+});
+});
