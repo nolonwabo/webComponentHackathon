bc170ff258a256bd2f7d67e8c0671e4e5d768a87
Amandankosiyane <amandankosiyane.etafeni@gmail.com>
2017-11-22 11:28:57 +0200

    made Post ajax call to add new books

--- src/js/jet-composites/book-comp/viewModel.js
+++ src/js/jet-composites/book-comp/viewModel.js
@@ -14,29 +14,93 @@ define(
       this.description = ko.observable(data.description);
       this.available_Books = ko.observable(data.available_Books);
     }
-
-    //return new DashboardViewModel();
     function BookModel() {
       var self = this;
+      self.books = ko.observableArray([]);
 
       $.getJSON("https://immense-refuge-39063.herokuapp.com/api/booklist", function(allData) {
-        console.log(allData);
+        // console.log(allData);
         var mappedTasks = $.map(allData, function(item) {
           return new Task(item)
         });
         self.books(mappedTasks);
       });
 
-      self.books = ko.observableArray([]);
 
     }
 
     return new BookModel();
 
+function addNewBooks(){
+        var self = this;
+        self.addedBooks = ko.observableArray([]);
+
+        $.getJSON("https://immense-refuge-39063.herokuapp.com/api/booklist", function(shoesAdded){
+                // console.log("-----------", shoesAdded);
+                var mappedShoes = $.map(shoesAdded, function(shoe){
+                        return new Task( shoe)
+                });
+                self.addedBooks(mappedShoes);
+        });
+}
+
   });
 
 
 
+
+  // function Guest(data){
+  //     this.id = ko.observable(data.id);
+  //     this.name = ko.observable(data.name);
+  //     this.email = ko.observable(data.email);
+  //     this.guests = ko.observable(data.guests);
+  //     this.code = ko.observable(data.code);
+  // }
+  //
+  // function guestListViewModel(){
+  //     //Data
+  //     var self = this;
+  //     self.guests = ko.observableArray([]);
+  //     self.guestsNumber = 0;
+  //
+  //     $.getJSON('/php/guests_json.php', function(json) {
+  //         var mappedGuests = $.map(json, function(item) { return new Guest(item) });
+  //         self.guests(mappedGuests);
+  //         self.guestsNumber = (self.guests().length);
+  //         $('.dlt_btn').button();
+  //
+  //     });
+  //
+  //     self.removeGuest = function(guest) {
+  //         self.guests.destroy(guest);
+  //     };
+  //
+  //     self.save = function() {
+  //         var data = 'json=' + ko.toJSON({guests: self.guests });
+  //         $.ajax("/php/save_guests.php", {
+  //             data: data,
+  //             type: "post",
+  //             success: function(result) {$('#server').html(result)}
+  //         })
+  //     }
+  //
+  //     self.totalGuests = ko.computed(function() {
+  //         var total = 0;
+  //         ko.utils.arrayForEach(self.guests(), function(guest) {
+  //             var value = guest.id;
+  //             console.log(value);
+  //             if (!isNaN(value)) {
+  //                 total += value;
+  //             }
+  //         });
+  //         return total;
+  //     });
+  //
+  // }
+  //
+  // ko.applyBindings(new guestListViewModel);
+  //
+  //
 
 
     // function ExampleComponentModel(context) {
