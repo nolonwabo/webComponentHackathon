0da51a0d267f21032902038715ad88ca176e78b8
sivuyisiwe <sivuyisiwequse0@gmail.com>
2017-11-22 08:51:12 +0200

    initial commit

---
+++ src/js/jet-composites/book-comp/viewModel.js
@@ -0,0 +1,40 @@
+/**
+  Copyright (c) 2015, 2017, Oracle and/or its affiliates.
+  The Universal Permissive License (UPL), Version 1.0
+*/
+define(
+    ['ojs/ojcore', 'knockout', 'jquery'], function (oj, ko, $) {
+    'use strict';
+
+    function ExampleComponentModel(context) {
+        var self = this;
+        self.composite = context.element;
+        //Example observable
+        // self.messageText = ko.observable('Hello from Example Component');
+        // self.title = ko.observable("Beauty and the Beast");
+        // self.description = ko.observable("Belle, a bright, beautiful and independent young woman, is taken prisoner by a beast in its castle. Despite her fears, she befriends the castle's enchanted staff and learns to look beyond the beast's hideous exterior, allowing her to recognize the kind heart and soul of the true prince that hides on the inside.")
+
+        context.props.then(function (propertyMap) {
+            //Store a reference to the properties for any later use
+            self.properties = propertyMap;
+
+            //Parse your component properties here
+
+        });
+    };
+
+    //Lifecycle methods - uncomment and implement if necessary
+    //ExampleComponentModel.prototype.activated = function(context){
+    //};
+
+    //ExampleComponentModel.prototype.attached = function(context){
+    //};
+
+    //ExampleComponentModel.prototype.bindingsApplied = function(context){
+    //};
+
+    //ExampleComponentModel.prototype.detached = function(context){
+    //};
+
+    return ExampleComponentModel;
+});
