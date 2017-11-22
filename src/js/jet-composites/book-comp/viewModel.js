337a0d9bf87f14449de227a0c6d7bb9739dd4c2b
Amandankosiyane <amandankosiyane.etafeni@gmail.com>
2017-11-22 10:01:08 +0200

    Made an ajax call to display all the books

--- src/js/jet-composites/book-comp/viewModel.js
+++ src/js/jet-composites/book-comp/viewModel.js
@@ -6,32 +6,65 @@ define(
     ['ojs/ojcore', 'knockout', 'jquery'], function (oj, ko, $) {
     'use strict';
 
-    function ExampleComponentModel(context) {
-        var self = this;
-        self.composite = context.element;
-        //Example observable
-        
-        context.props.then(function (propertyMap) {
-            //Store a reference to the properties for any later use
-            self.properties = propertyMap;
 
-            //Parse your component properties here
+    function Task(data) {
+            this.image = ko.observable(data.image);
+      this.title = ko.observable(data.title);
+      this.author = ko.observable(data.author);
+      this.description = ko.observable(data.description);
+      this.available_Books = ko.observable(data.available_Books);
+    }
 
+    //return new DashboardViewModel();
+    function BookModel() {
+      var self = this;
+
+      $.getJSON("https://immense-refuge-39063.herokuapp.com/api/booklist", function(allData) {
+        console.log(allData);
+        var mappedTasks = $.map(allData, function(item) {
+          return new Task(item)
         });
-    };
+        self.books(mappedTasks);
+      });
 
-    //Lifecycle methods - uncomment and implement if necessary
-    //ExampleComponentModel.prototype.activated = function(context){
-    //};
+      self.books = ko.observableArray([]);
 
-    //ExampleComponentModel.prototype.attached = function(context){
-    //};
+    }
 
-    //ExampleComponentModel.prototype.bindingsApplied = function(context){
-    //};
+    return new BookModel();
 
-    //ExampleComponentModel.prototype.detached = function(context){
-    //};
+  });
 
-    return ExampleComponentModel;
-});
+
+
+
+
+    // function ExampleComponentModel(context) {
+    //     var self = this;
+    //     self.composite = context.element;
+    //     //Example observable
+    //
+    //     context.props.then(function (propertyMap) {
+    //         //Store a reference to the properties for any later use
+    //         self.properties = propertyMap;
+    //
+    //         //Parse your component properties here
+    //
+    //     });
+    // };
+    //
+    // //Lifecycle methods - uncomment and implement if necessary
+    // //ExampleComponentModel.prototype.activated = function(context){
+    // //};
+    //
+    // //ExampleComponentModel.prototype.attached = function(context){
+    // //};
+    //
+    // //ExampleComponentModel.prototype.bindingsApplied = function(context){
+    // //};
+    //
+    // //ExampleComponentModel.prototype.detached = function(context){
+    // //};
+    //
+    // return ExampleComponentModel;
+// });
--- src/js/viewModels/dashboard.js
+++ src/js/viewModels/dashboard.js
@@ -2,21 +2,30 @@ define(['ojs/ojcore', 'knockout', 'jquery', 'ais', 'ds', 'jet-composites/filter-
   function (oj, ko, $, ais, ds) {
 
 
-  // return showAllBooksViewModel();
-    function DashboardViewModel() {
-      var self = this;
-      // check ds is initialized
-      self.itemsArray = ko.observable([]);
-      // for web component pattern
-      // data service delivers for filter-table component --- data prop requirements
-      self.title = ko.observable("Beauty and the Beast");
-      self.description = ko.observable("Belle, a bright, beautiful and independent young woman, is taken prisoner by a beast in its castle. Despite her fears, she befriends the castle's enchanted staff and learns to look beyond the beast's hideous exterior, allowing her to recognize the kind heart and soul of the true prince that hides on the inside.")
-      self.image = ko.observable("https://www.bookdepository.com/Disney-Beauty-Beast-Magical-Story-with-Amazing-Moving-Picture-Cover-Disney/9781445464817");
-      self.itemData = ds.getItemsStatic();
-      ds.init().then(function (data){
-        console.log(data)
-        // for JQUERY injection pattern - below in hamdleAttached method
-})
-}
+          function Task(data) {
+                  this.image = ko.observable(data.image);
+            this.title = ko.observable(data.title);
+            this.author = ko.observable(data.author);
+            this.description = ko.observable(data.description);
+            this.available_Books = ko.observable(data.available_Books);
+          }
 
-});
+          //return new DashboardViewModel();
+          function BookModel() {
+            var self = this;
+
+            $.getJSON("https://immense-refuge-39063.herokuapp.com/api/booklist", function(allData) {
+              console.log(allData);
+              var mappedTasks = $.map(allData, function(item) {
+                return new Task(item)
+              });
+              self.books(mappedTasks);
+            });
+
+            self.books = ko.observableArray([]);
+
+          }
+
+          return new BookModel();
+
+        });
